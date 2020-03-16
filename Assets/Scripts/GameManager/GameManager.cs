3320595b23edafdec5680885b29d6a36629a181f
Stale Cookie <jamesbakerpublic@gmail.com>
2020-03-16 18:48:03 +1030

    - Added wheat material - Added Main Level 2 - Addeed farmer stuff (WIP) - Added Building Scripts - Added GameManager

---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> FarmerHouses = new List<GameObject>();
+    public List<GameObject> AI = new List<GameObject>();
+
+    private int HouseNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i <AI.Count; i++)
+        {
+            float spacing = -2;
+            Vector3 pos = new Vector3(spacing * 1, 0, 0);
+            Instantiate(AI[i], pos, Quaternion.identity);
+            AssignFarmerHouse();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AssignFarmerHouse()
+    {
+        FarmerHouses = GameObject.FindGameObjectsWithTag("Farmer House").ToList();
+
+        if(FarmerHouses.Count >= 1)
+        {
+            foreach(GameObject item in FarmerHouses)
+            {
+
+                HouseNum++;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/NPCs/AI/Farmer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Farmer : MonoBehaviour
+{
+    public LayerMask farmableLayer;
+
+    private GameObject House;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.SphereCast(House.gameObject.transform.position, 300, Vector3.up, out hit, farmableLayer))
+        {
+
+        }
+    }
+}
