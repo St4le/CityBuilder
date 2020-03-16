7c9bd63c7a97025505641100614984394330ab8a
LiteralGarbage <hudson1lewis@icloud.com>
2020-03-16 20:24:29 +1030

    road and textures

--- Assets/Scripts/Placement/GroundPlacementSystem.cs
+++ Assets/Scripts/Placement/GroundPlacementSystem.cs
@@ -73,7 +73,7 @@ public class GroundPlacementSystem : MonoBehaviour
         if(structure != null)
         {
             truePos.x = Mathf.Floor(target.transform.position.x / gridSize) * gridSize;
-            truePos.y = Mathf.Floor(target.transform.position.y / gridSize) * gridSize;
+            truePos.y = target.transform.position.y;//Mathf.Floor(target.transform.position.y / gridSize) * gridSize;
             truePos.z = Mathf.Floor(target.transform.position.z / gridSize) * gridSize;
 
             structure.transform.position = truePos;
